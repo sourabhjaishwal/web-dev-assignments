cf6bf476b5685c9a245ff010fede2eef1a962af1
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2024-12-25 19:55:44 +0530

    5 challenges on conditionals

---
+++ 04-JavaScript/practice/01_conditionals.js
@@ -0,0 +1,57 @@
+// Challenges on Conditionals
+
+// Checking if a number is greater than another number.
+/*
+let num1 = 5;
+let num2 = 8;
+
+if (num1 >= num2) {
+console.log('Num1 is greater than Num2.');
+} else {
+console.log('Num1 is smaller than Num2');
+}
+*/
+
+// Checking if a string is equal to another string.
+/*
+let username = "chai";
+let anotherUsername = "chai";
+
+if (username == anotherUsername) {
+console.log("Pick another Username");
+} else {
+console.log("You can pick this username")
+}
+*/
+
+// Checking if a variable is a number or not.
+/*
+let score = 20;
+
+if (typeof score === 'number') {
+console.log("Yes, this is a number")
+} else {
+console.log("No, it's not a number")
+}
+*/
+
+// Checking if a boolean value is true or false.
+/*
+let isCoffeeReady = true;
+
+if (isCoffeeReady) {
+console.log("Coffee is Ready!")
+} else {
+console.log("Coffee is not Ready!")
+}
+*/
+
+// Checking if an array is empty or not.
+
+let items = [1, 2, 3];
+
+if (items.length === 0) {
+  console.log("Array is empty!");
+} else {
+  console.log("Array is not empty!");
+}
