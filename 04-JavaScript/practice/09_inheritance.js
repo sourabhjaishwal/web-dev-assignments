5ec612bac4de2b973662f35c92547f7848b9c093
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2025-01-30 22:32:47 +0530

    classes and inhertance

---
+++ 04-JavaScript/practice/09_inheritance.js
@@ -0,0 +1,27 @@
+// Inheritance in JS
+
+// classes
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  start() {
+    return `${this.model} is a car from ${this.make} from the year ${this.year}`;
+  }
+}
+
+// Inheritance
+class Car extends Vehicle {
+  drive() {
+    return `${this.make} : This is an inheritance example!`;
+  }
+}
+
+let myCar = new Car("Toyota", "Etios", 2021);
+console.log(myCar.start());
+console.log(myCar.drive());
+
+let newCar = new Vehicle("Tata", "Safari", 2018);
+console.log(newCar.make);
