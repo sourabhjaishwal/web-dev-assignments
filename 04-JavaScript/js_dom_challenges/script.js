d1808921c675afd21d0940ff212cfaa82d1099af
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2025-02-08 22:42:49 +0530

    five more challenges on dom

--- 04-JavaScript/js_dom_challenges/script.js
+++ 04-JavaScript/js_dom_challenges/script.js
@@ -22,7 +22,7 @@ document
 
 document.getElementById("changeOrder").addEventListener("click", function () {
   let coffeeType = document.getElementById("coffeeType");
-  //   console.log(coffeeType);
+  // console.log(coffeeType);
   coffeeType.textContent = "Espresso";
   coffeeType.style.backgroundColor = "#3c2218";
   coffeeType.style.color = "white";
@@ -34,7 +34,7 @@ document.getElementById("changeOrder").addEventListener("click", function () {
 document.getElementById("addNewItem").addEventListener("click", function () {
   let newItem = document.createElement("li");
   newItem.textContent = "Eggs";
-  //   console.log(newItem);
+  // console.log(newItem);
   document.getElementById("shoppingList").appendChild(newItem);
 });
 
@@ -45,4 +45,49 @@ document
   .addEventListener("click", function () {
     let tasks = document.getElementById("taskList");
     tasks.lastElementChild.remove();
+  });
+
+// example - 6
+
+document
+  .getElementById("clickMeButton")
+  .addEventListener("dblclick", function () {
+    alert("chaicode");
+  });
+
+// example - 7
+
+document.getElementById("teaList").addEventListener("click", function (event) {
+  // console.log(event.target)
+  if (event.target && event.target.matches(".teaItem")) {
+    alert("You selected: " + event.target.textContent);
+  }
+});
+
+// example - 8
+
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let feedback = document.getElementById("feedbackInput").value;
+    console.log(feedback);
+    document.getElementById(
+      "feedbackDisplay"
+    ).textContent = `Feedback is: ${feedback}`;
+  });
+
+// example - 9
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "DOM Fully Loaded";
+});
+
+// example - 10
+
+document
+  .getElementById("toggleHighlight")
+  .addEventListener("click", function () {
+    let descriptionText = document.getElementById("descriptionText");
+    descriptionText.classList.toggle("highlight");
   });
