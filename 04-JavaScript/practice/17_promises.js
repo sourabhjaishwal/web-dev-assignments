aa2a4575d2e0aa0f989ee8272ae0dfbc8f9196c7
sourabhjaishwal <jaishwalsourabh2000@gmail.com>
2025-02-18 21:53:14 +0530

    promises in js

---
+++ 04-JavaScript/practice/17_promises.js
@@ -0,0 +1,26 @@
+// Promises - utility in javascript
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let success = true;
+      if (success) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Error fetching data");
+      }
+    }, 3000);
+  });
+}
+
+fetchData()
+  .then((data) => {
+    console.log(data);
+    return `Hitesh`;
+  })
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
