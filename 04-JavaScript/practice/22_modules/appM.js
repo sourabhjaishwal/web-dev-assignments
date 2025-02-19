2f53e7a54a58a0eee0d61c35e44a209780c29744
sourabhjaishwal <jaishwalsourabh2000@gmail.com>
2025-02-19 22:12:29 +0530

    es6 modules example

---
+++ 04-JavaScript/practice/22_modules/appM.js
@@ -0,0 +1,7 @@
+// Module import file
+
+import multiply from "./operationM.js"; // default import
+import { add, subtract } from "./operationM.js"; // named import
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
---
+++ 04-JavaScript/practice/22_modules/operationM.js
@@ -0,0 +1,15 @@
+// ES6 Modules
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}
+
+// create a separate file with name as - appM.js
